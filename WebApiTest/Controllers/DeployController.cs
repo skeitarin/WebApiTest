4424f75edef93c6412a7a6f77251f266fd66614e
Keita <gk19901020@gmail.com>
2016-11-06 09:12:51 +0900

    first commit

---
+++ WebApiTest/Controllers/DeployController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace WebApiTest.Controllers
+{
+    public class DeployController : Controller
+    {
+        // GET: Deploy
+        public ActionResult Success()
+        {
+            return View();
+        }
+    }
+}
